ccb790db0d58f0ff5ae8a136e1b80d128ddc71e5
Chukuigwe <M.nosakhare@rgu.ac.uk>
2024-03-17 17:37:53 +0100

    - created database schema - created login functionlity - updated REadme

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php 
-  require('./inc/db_config.php')
+  require('./inc/db_config.php');
+  require('./inc/utils.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,27 +30,36 @@
             </div>
             <button name="login" type="submit"> Log In </button>
           </form>
+          <?php 
+            if(isset($_POST['login'])) {
+              $from_data = filteration($_POST);
+              // $checkIfUserExist = "SELECT EXISTS(SELECT 1 FROM customers where email = $from_data)";
+              
+              $query = "SELECT * FROM customer where `email` =? AND `password` =? ";
+              $values = [$from_data['email'], $from_data['password']];
+              $res = select($query, $values, "ss");
+              if($res->num_rows ==1 ) {
+                $row = mysqli_fetch_assoc($res);
+                session_start();
+                $_SESSION['isAuthenticated'] = true;
+                $_SESSION['customerId'] = $row['cid'];
+                redirect('/dashboard.php');
+              } else {
+                echo ' 
+                    <div class="alert alert-warning" role="alert">
+                      Wrong user credentials
+                    </div>
+                  ';
+              }
+            }
+          ?>
           <hr />
             <span> Forgot password </span>
 
           <div> Don't have an account ? <span><a href="./signup.php"> Sign up </a></span></div>
     </section>
     <!-- login from section ends-->
-    <?php 
-      if(isset($_POST['login'])) {
-        $from_data = filteration($_POST);
-        echo"<h1> $from_data[email] </h1> ";
-        echo"<h1> $from_data[password] </h1> ";
-
-        // $checkIfUserExist = "SELECT EXISTS(SELECT 1 FROM customers where email = $from_data)";
-        
-        $query = "SELECT * FROM customer where `customer_email` =? AND `customer_pass` =? ";
-        $values = [$from_data['email'], $from_data['password']];
-        
-        select($query, $values, "ss");
-      }
-    ?>
-      <?php require('./inc/footer.php') ?>
+              <?php require('./inc/footer.php') ?>
   </body>
   <?php require('./inc/script.php') ?>
 </html>
